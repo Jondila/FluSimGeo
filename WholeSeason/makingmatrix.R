5c418c1f2c37ba689a10fccc25c96f2ba10dee92
Tim Vaughan <tgvaughan@gmail.com>
2016-10-14 11:53:23 +1300

    Added infection rate calculation.

--- WholeSeason/makingmatrix.R
+++ WholeSeason/makingmatrix.R
@@ -1,24 +1,26 @@
-Newdata <- read.csv(file = 'RTOPOP.csv')
-Newdata <-Newdata[!grepl("Area Outside RTO", Newdata$rtonames),]
-A <- Newdata$popsize
-df <- read.csv(file = 'RTODATA.csv', as.is=T)
-df
-df <- df[!grepl("Area Outside RTO", df$Home_RTO),]
-df <- df[!grepl("Area Outside RTO", df$RTO),]
-df <- df[!grepl("NULL", df$RTO),]
-RTO_Names <- unique(df$RTO)
-Home_names <- unique(df$Home_RTO)
-df$Rate[df$Home_RTO=="Auckland RTO"]
-sum(df$Rate[df$Home_RTO=="Dunedin RTO"])
-for (name in RTO_Names) {cat(name)}
-N <- length(Home_names)
+## Load data
 
-for (name in Home_names){
-  s <- sum(df$Rate[df$Home_RTO==name])
-  cat(paste(s,"\n"))
-}
+dfPopSizes <- read.csv(file = 'RTOPOP.csv', as.is=T)
+dfTP <- read.csv(file = 'RTODATA.csv', as.is=T)
 
-P <- matrix(0, nrow = N, ncol = N)
+## Remove Area Outside and NULL
+
+dfPopSizes <-dfPopSizes[!grepl("Area Outside RTO", dfPopSizes$rtonames),]
+dfTP <- df[!grepl("Area Outside RTO", df$Home_RTO),]
+dfTP <- df[!grepl("Area Outside RTO", df$RTO),]
+dfTP <- df[!grepl("NULL", df$RTO),]
+
+## Assemble list of names and sort
+
+RTO_Names <- sort(unique(dfTP$RTO))
+N <- length(RTO_Names)
+
+dfPopSizes <- dfPopSizes[order(dfPopSizes$rtonames),]
+RTO_PopSizes <- dfPopSizes$popsize
+
+## Compute epsilon matrix
+
+epsilon <- matrix(0, nrow = N, ncol = N)
 
 for (i in 1:N) {
   iName <- RTO_Names[i]
@@ -28,22 +30,30 @@ for (i in 1:N) {
 
     jName <- RTO_Names[j]
 
-    recIdx <- which(df$Home_RTO==iName & df$RTO ==jName)
+    recIdx <- which(dfTP$Home_RTO==iName & dfTP$RTO ==jName)
     if (length(recIdx)>0) {
-      P[i,j] <- df$Rate[recIdx]
+      epsilon[i,j] <- dfTP$Rate[recIdx]
     }
   }
 }
 
-diag(P) <- 1 - rowSums(P)
+diag(epsilon) <- 1 - rowSums(epsilon)
 
-N <- length(A)
-for (i in N){
- ino <- A[i]
-for (j in N) {
-ipo <- diag(P)[i]
-if(i==j){
-beta[i] <- 1.2/((ino-1) * ipo)
+## Compute reaction rates
+
+R0 <- 1.2
+infectiousPeriod <- 1 # week
+
+recoveryRate <- 1/infectiousPeriod
+
+beta <- rep(0, N)
+for (i in 1:N) {
+    beta[i] <- R0*recoveryRate/(epsilon[i,i]*RTO_PopSizes[i])
+}
+
+infectionRate <- matrix(0, nrow=N, ncol=N)
+for (i in 1:N) {
+    for (j in 1:N) {
+        infectionRate[i,j] <- beta[j]*epsilon[i,j]
     }
-  }
 }
